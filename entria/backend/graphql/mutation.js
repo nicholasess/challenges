496ba46135b6f10f5747d10c9be3f80d7821e96c
user <user@users-Mac-mini.local>
2018-04-11 10:13:54 -0300

    add fullstack folder

--- entria/backend/graphql/mutation.js
+++ entria/backend/graphql/mutation.js
@@ -9,8 +9,32 @@ module.exports = {
     args: {
       title: { type: new graphql.GraphQLNonNull(graphql.GraphQLString) }
     },
-    resolve: (_, { title }) => {
-      return { data: "ok" };
+    resolve: async (_, { title }) => {
+      const newitem = new Model({ title: title });
+      return await newitem.save();
+    }
+  },
+  update: {
+    type: GraphModel,
+    description: "Atualizando um item",
+    args: {
+      title: { type: new graphql.GraphQLNonNull(graphql.GraphQLString) },
+      _id: { type: new graphql.GraphQLNonNull(graphql.GraphQLString) }
+    },
+    resolve: async (_, { title }) => {
+      const newitem = new Model({ title: title });
+      return await newitem.save();
+    }
+  },
+  delete: {
+    type: GraphModel,
+    description: "Excluindo um item",
+    args: {
+      _id: { type: new graphql.GraphQLNonNull(graphql.GraphQLString) }
+    },
+    resolve: async (_, { title }) => {
+      const newitem = new Model({ title: title });
+      return await newitem.save();
     }
   }
 };
